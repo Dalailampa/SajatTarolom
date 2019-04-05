721814a80bd9bd4fd065f521a1f93b4b1eb62bab
Dalailampa <turan.laszlo@gmail.com>
2019-04-05 18:45:32 +0200

    aaa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AppComponent} from './app.component';
+import {PageNotFoundComponent} from './core/page-not-found/page-not-found.component';
+import {AboutComponent} from './about/about.component';
+import {BookComponent} from './book/book.component';
+import {EventComponent} from './event/event.component';
+import {MenuComponent} from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'app', component: AppComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'book', component: BookComponent},
+  {path: 'event', component: EventComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: '', redirectTo: '../src/index.html', pathMatch: 'full'},
+   {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  static routableComponents = [
+    AppComponent,
+    AboutComponent,
+    EventComponent,
+    BookComponent,
+    PageNotFoundComponent
+  ];
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {PageNotFoundComponent} from './core/page-not-found/page-not-found.component';
+import {BannerComponent} from './core/banner/banner.component';
+import {FooterComponent} from './core/footer/footer.component';
+import {AboutComponent} from './about/about.component';
+import { EventComponent } from './event/event.component';
+import { BookComponent } from './book/book.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    ...AppRoutingModule.routableComponents,
+    PageNotFoundComponent,
+    BannerComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +25,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
